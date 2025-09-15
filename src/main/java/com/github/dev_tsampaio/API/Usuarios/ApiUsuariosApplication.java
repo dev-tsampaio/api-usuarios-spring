a4c406e7d7ef2ebf02b06814d54f3215fc11a579
dev-tsampaio <thalessampaiovp@icloud.com>
2025-09-15 12:26:48 -0300

    docs: Criado novo projeto e adicionado ao GitHub

---
+++ src/main/java/com/github/dev_tsampaio/API/Usuarios/ApiUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.github.dev_tsampaio.API.Usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUsuariosApplication.class, args);
+	}
+
+}
